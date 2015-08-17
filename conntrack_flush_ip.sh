8aa30e3e6a395281ac6882e4cb50269f5cad4cfd
Jetsada Machom <zinuzoid@gmail.com>
2015-08-17 11:09:48 +0700

    Update conntrack_flush_ip.sh

--- conntrack_flush_ip.sh
+++ conntrack_flush_ip.sh
@@ -4,39 +4,42 @@
 IP=$1
 
 if [[ "$1" == "" ]]; then
-        echo 'ip required'
-        exit 1
+	echo 'ip required'
+	exit 1
 fi
 
 conntrack -p udp -s $IP -L | \
-        while read proto _ _ src dst sport dport _; do
-                PROTO=`echo "$proto" | cut -d "=" -f2`
-                SRC=`echo "$src" | cut -d "=" -f2`
-                DST=`echo "$dst" | cut -d "=" -f2`
-                SPORT=`echo "$sport" | cut -d "=" -f2`
-                DPORT=`echo "$dport" | cut -d "=" -f2`
+	while read proto _ _ src dst sport dport _; do
+		PROTO=`echo "$proto" | cut -d "=" -f2` 
+		SRC=`echo "$src" | cut -d "=" -f2` 
+		DST=`echo "$dst" | cut -d "=" -f2` 
+		SPORT=`echo "$sport" | cut -d "=" -f2` 
+		DPORT=`echo "$dport" | cut -d "=" -f2` 
 
-                conntrack -D \
-                        --proto $PROTO \
-                        --orig-src $SRC \
-                        --orig-dst $DST \
-                        --sport $SPORT \
-                        --dport $DPORT
-        done
+		conntrack -D \
+			--proto $PROTO \
+			--orig-src $SRC \
+			--orig-dst $DST \
+			--sport $SPORT \
+			--dport $DPORT
+	done
 
 
 conntrack -p tcp -s $IP -L | \
-        while read proto _ _ _ src dst sport dport _; do
-                PROTO=`echo "$proto" | cut -d "=" -f2`
-                SRC=`echo "$src" | cut -d "=" -f2`
-                DST=`echo "$dst" | cut -d "=" -f2`
-                SPORT=`echo "$sport" | cut -d "=" -f2`
-                DPORT=`echo "$dport" | cut -d "=" -f2`
+	while read proto _ _ _ src dst sport dport _; do
+		PROTO=`echo "$proto" | cut -d "=" -f2` 
+		SRC=`echo "$src" | cut -d "=" -f2` 
+		DST=`echo "$dst" | cut -d "=" -f2` 
+		SPORT=`echo "$sport" | cut -d "=" -f2` 
+		DPORT=`echo "$dport" | cut -d "=" -f2` 
 
-                conntrack -D \
-                        --proto $PROTO \
-                        --orig-src $SRC \
-                        --orig-dst $DST \
-                        --sport $SPORT \
-                        --dport $DPORT
-        done
+		conntrack -D \
+			--proto $PROTO \
+			--orig-src $SRC \
+			--orig-dst $DST \
+			--sport $SPORT \
+			--dport $DPORT
+	done
+
+echo "FLUSH $IP CONNTRACK"
+echo "`date` FLUSH $IP CONNTRACK" >> /tmp/zlog
